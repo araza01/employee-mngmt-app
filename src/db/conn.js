e7bc2acb20487b856d9093ec7556e6460e9fdc0d
amir <amirraza1794@gmail.com>
2021-01-11 23:36:14 +0530

    Change db user

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require(`mongoose`);
 
-mongoose.connect("mongodb+srv://project1:pass123@cluster0.8ehmh.mongodb.net/test?retryWrites=true&w=majority", { 
+mongoose.connect("mongodb+srv://employee:pass123@cluster0.8ehmh.mongodb.net/test?retryWrites=true&w=majority", { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true,
